3de7f52ac818eb6d020fd7ad787e754581943723
chaixingzhi <chai@lingdao.com>
2018-10-27 21:29:46 +0800

    12

--- node_wechat/libs/tpl.js
+++ node_wechat/libs/tpl.js
@@ -18,11 +18,11 @@ var tpl = heredoc(function() {/*
             </Image>
         <% } else if (MsgType === 'voice') { %>
             <Voice>
-                <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
+                <MediaId><![CDATA[<%= content.mediaId %>]]></MediaId>
             </Voice>
          <% } else if (MsgType === 'video') { %>
             <Video>
-                <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
+                <MediaId><![CDATA[<%= content.mediaId %>]]></MediaId>
                 <Title><![CDATA[<%= content.title %>]]></Title>
                 <Description><![CDATA[<%= content.description %>]]></Description>
             </Video>
